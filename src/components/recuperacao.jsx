9c0acb36470726744e03a1105ae27104330296e0
Hanrry <hanrryravel159357@gmail.com>
2026-05-02 13:53:19 -0300

    Docs:recuperação de senha

---
+++ src/components/recuperacao.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const enviarEmail = async () => {
+    if (!email) {
+      alert("Digite um email válido");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      await emailjs.send(
+        "service_iw0xk2c",
+        "template_mgoiai8",
+        {
+          to_email: email,
+          message: "Clique aqui para recuperar sua senha"
+        },
+        "7K2SI7Cmb0RSq5SrH"
+      );
+
+      alert("Se tiver algum e-mail vinculado a este, será enviado um email de recuperação");
+    } catch (error) {
+      console.error("ERRO COMPLETO:", error);
+      alert("Erro ao enviar email");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Recuperar senha</h1>
+
+      <input
+        type="email"
+        placeholder="Digite seu email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <button onClick={enviarEmail} disabled={loading}>
+        {loading ? "Enviando..." : "Recuperar senha"}
+      </button>
+    </div>
+  );
+}
+
+export default App;
